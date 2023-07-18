7f5166018294d1af430ce3e2ab23026afb6cb176
Shohanul Alam <shohan12224943@gmail.com>
2023-07-18 07:53:18 +0600

    Update grading.js

--- grading.js
+++ grading.js
@@ -1,69 +1,31 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'gradingStudents' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY grades as parameter.
- */
-
 function gradingStudents(grades) {
-    for(let i = 0; i < grades.length; i++)
-    {
-        console.log(grades[i] % 5)
-        if(grades[i] < 40)
-        {
-            console.log(grades[i])
-        }
-        if(grades[i] % 5 == 3)
-        {
-            console.log(grades[i] + 2)
-        }
-        else if(grades[i] % 5 == 4)
-        {
-            console.log(grades[i] + 1)
-        }
+  const roundedGrades = [];
+
+  for (let i = 0; i < grades.length; i++) {
+    const grade = grades[i];
+    //console.log("grade" + grade)
+    if (grade < 40) {
+      grade = 40;
+      roundedGrades.push(grade);
+    } else {
+        
+      //console.log("greater than 40")
+      const remainder = grade % 5;
+      //console.log("remainder " + remainder)
+      
+      const nextMultipleOf5 = grade + (5 - remainder);
+      //console.log("nextMultipleOf5 " + nextMultipleOf5)
+
+      if (remainder > 2) {
+          //console.log("less than 3")
+        roundedGrades.push(nextMultipleOf5);
+      } else {
+          //console.log("not less than 3")
+        roundedGrades.push(grade);
+      }
     }
-}
+  }
 
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const gradesCount = parseInt(readLine().trim(), 10);
-
-    let grades = [];
-
-    for (let i = 0; i < gradesCount; i++) {
-        const gradesItem = parseInt(readLine().trim(), 10);
-        grades.push(gradesItem);
-    }
-
-    const result = gradingStudents(grades);
-
-    ws.write(result.join('\n') + '\n');
-
-    ws.end();
+  //console.log(roundedGrades)
+  return roundedGrades;
 }
